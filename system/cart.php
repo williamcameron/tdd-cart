bab38c9d9f43c0f59e77175589f6df929f0ade88
William Cameron <william.cameron@mtcmedia.co.uk>
2014-05-23 21:46:18 +0100

    First Cart test, folder structure and the like.. this may change

---
+++ system/cart.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Cart is the base shopping cart class of my TDD Cart
+ *
+ * @author William Cameron
+ */
+class Cart {
+    public $lines = array();
+}
+
--- tests/unit/cartTest.php
+++ tests/unit/cartTest.php
@@ -1,6 +1,14 @@
 <?php
-	class cartTest extends PHPUnit_Framework_Testcase{
-		public function testPhpUnitSanityCheck(){
-			$this->assertTrue(true);
-		}
-	}
+    include dirname(__FILE__)."/../../system/cart.php";
+    
+class cartTest extends PHPUnit_Framework_Testcase {
+
+    public function testPhpUnitSanityCheck() {
+        $this->assertTrue(true);
+    }
+    
+    public function test_NewCart_GivenNoParameters_ReturnsEmptyCart(){
+        $cart = new Cart();
+        $this->assertEquals(0, count($cart->lines));
+    }
+}
